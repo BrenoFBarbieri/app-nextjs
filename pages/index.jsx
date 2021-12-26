bfac03a03a72c6ff9540e3d6b45841ca4112bb77
Breno Barbieri <breno.barbieri@kumulus.com.br>
2021-12-25 21:06:15 -0300

    Dynamic Routes

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,10 @@
+import Link from 'next/link'
+import Date from '../components/date'
 import Layout from '../components/layout'
-import utilStyles from '../styles/utils.module.css'
+
 import { getSortedPostsData } from '../lib/posts'
+
+import utilStyles from '../styles/utils.module.css'
 
 export async function getStaticProps() {
   const allPostsData = getSortedPostsData()
@@ -22,11 +26,13 @@ export default function Home({ allPostsData }) {
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
-              {title}
+              <Link href={`/posts/${id}`}>
+                <a>{title}</a>
+              </Link>
               <br />
-              {id}
-              <br />
-              {date}
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
             </li>
           ))}
         </ul>
